62c585fbc89031404a0a069aa0f1a2edc1e35175
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-07-21 08:29:15 -0300

    feat: add ms template

---
+++ src/config/server/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import env from '../env/env';
+import { setupRoutes } from '@/config/server/setup-routes';
+import '@/config/db/db';
+
+const app = express();
+const port = env.server.port;
+
+app.use(express.json());
+app.use('/api', setupRoutes());
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
+});
+
+export default app;
