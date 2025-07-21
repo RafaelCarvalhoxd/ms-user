a4c75ea2042272057dba69f5c395767297e7c853
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-07-21 17:36:39 -0300

    feat: add user schema

--- src/config/db/schemas/user.ts
+++ src/config/db/schemas/user.ts
@@ -5,7 +5,6 @@ export const users = pgTable('users', {
   fullName: varchar('full_name', { length: 100 }).notNull(),
   email: varchar('email', { length: 150 }).notNull().unique(),
   passwordHash: text('password_hash').notNull(),
-  phoneNumber: varchar('phone_number', { length: 20 }),
   birthDate: date('birth_date'),
   isEmailVerified: boolean('is_email_verified').notNull().default(false),
   emailVerificationToken: text('email_verification_token'),
@@ -13,4 +12,6 @@ export const users = pgTable('users', {
   isActive: boolean('is_active').notNull().default(true),
   createdAt: timestamp('created_at', { withTimezone: false }).notNull().defaultNow(),
   updatedAt: timestamp('updated_at', { withTimezone: false }).notNull().defaultNow(),
+  phone_number: varchar('phone_number', { length: 20 }).notNull(),
+  gender: varchar('gender', { length: 10 }).notNull(),
 });
