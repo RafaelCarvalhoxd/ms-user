2c1a96e317ef2332eb28ab80b03d8022d5dc2c86
Fael <devrafaelcarvalho14@gmail.com>
2025-07-23 14:26:50 -0300

    chore: error handler

--- src/config/server/server.ts
+++ src/config/server/server.ts
@@ -3,12 +3,14 @@ import env from '../env/env';
 import { setupRoutes } from '@/config/server/setup-routes';
 import '@/config/db/db';
 import { startGrpcServer } from '../grpc/grpc';
+import { errorHandler } from './error-handler';
 
 const app = express();
 const port = env.server.port;
 
 app.use(express.json());
 app.use('/api', setupRoutes());
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`✅ Server is running on port ${port}`);
 });
